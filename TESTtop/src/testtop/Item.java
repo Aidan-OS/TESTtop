da2fff13fe5628bd9ef4660ab1cc25cd8680ce1c
Aidan Smith <LeoneShamoth@gmail.com>
2018-12-27 00:23:11 -0500

    Christmas is a busy time.

--- TESTtop/src/testtop/Item.java
+++ TESTtop/src/testtop/Item.java
@@ -3,10 +3,28 @@ package testtop;
  *  TESTtop. Making a better TTS for a happier experience.
  */
 
+import java.util.ArrayList;
+
 /**
  * @version 0.1
  * @author Leone Shamoth
  */
-public class Item {
+public class Item 
+{
+    // TODO: When creating the items, only list the regular items and have the ability to chose a +x version of each item through code
+    
+    private String name;
+    private boolean isMagic;
+    
+    private ArrayList<Attack> action;
+    private int attackBonus;
+    private int toHitBonus;
+    
+    private String description;
+    private String properties;
+    private double weight; //Weight in pounds
+    
+    private double cost; //Price is in gold
+    
     
 }
